6b7a19700618fdb54c28fcc52a6a08d565a75e21
calamity <planetarycalamity@gmail.com>
2022-03-23 16:01:02 +0100

    formatting and code fix

--- lacrimae.plugin.zsh
+++ lacrimae.plugin.zsh
@@ -1,4 +1,4 @@
-if [[-o interactive]] && [[-o login || -n "${LACRIMAE}"]]; then
+if [[ -o interactive ]] && [[ -o login || -n "${LACRIMAE}" ]]; then
     _cl_lacrimae=(
         #Templar chants
         "Ad te clamamus, exsules, filii Hevae.\n - Salve Regina"
@@ -8,9 +8,9 @@ if [[-o interactive]] && [[-o login || -n "${LACRIMAE}"]]; then
         #Gregorian chants
 
         #Warhammer chants
-        "Agitatis Ultramarini, \n Dominitis Ultramarini. \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
-        "Multum periculum in acie expectat \n Maximus animus sacrifici \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
-        "Sicut erat in principio \n et nunc et semper \n magni consili angelus \n - Why do I still live ? Vox-caster" 
+        "Agitatis Ultramarini, \nDominitis Ultramarini. \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
+        "Multum periculum in acie expectat\nMaximus animus sacrifici \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
+        "Sicut erat in principio\n et nunc et semper\n magni consili angelus\n - Why do I still live ? Vox-caster, Why do I still live ?" 
     )
 
     printf '%b\n\n' "${_cl_lacrimae[RANDOM % $#_cl_lacrimae + 1]}"
