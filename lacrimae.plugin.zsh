3ba1a4efa0e394a8faa762c9f34eab68ec90632f
caIamity <68069187+caIamity@users.noreply.github.com>
2022-03-25 11:47:45 +0100

    Update lacrimae.plugin.zsh

--- lacrimae.plugin.zsh
+++ lacrimae.plugin.zsh
@@ -17,7 +17,7 @@ if [[ -o interactive ]] && [[ -o login || -n "${LACRIMAE}" ]]; then
         #Warhammer chants
         "Agitatis Ultramarini, \nDominitis Ultramarini. \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
         "Multum periculum in acie expectat\nMaximus animus sacrifici \n - Ultramarine chant, Warhammer 40,000: Chaos Gate"
-        "Sicut erat in principio \net nunc et semper \nmagni consili angelus \n - Why do I still live ? Vox-caster, Why do I still live ?" 
+        "Sicut erat in principio \net nunc et semper \nmagni consili angelus \n - Why do I still live, Vox-caster" 
     )
 
     printf '%b\n\n' "${_cl_lacrimae[RANDOM % $#_cl_lacrimae + 1]}"
